7db6ae0627c46fc1e6510edb7c73d9087adaf803
임승현 <96limshyun@gmail.com>
2025-02-28 22:05:36 +0900

    feat: book 타입 정의

--- Frontend/src/types/index.ts
+++ Frontend/src/types/index.ts
@@ -1,1 +1,9 @@
 export type Title = "도서 등록" | "도서 수정"
+
+export interface BookType {
+    bookname: string;
+    authors: string;
+    publisher: string;
+    isbn13: string;
+    quantity: number;
+}
