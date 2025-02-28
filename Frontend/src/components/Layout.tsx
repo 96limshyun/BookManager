3487e7eacdfcebc157844066ecf58c989b730427
임승현 <96limshyun@gmail.com>
2025-02-28 12:52:47 +0900

    style: 공통 layout 스타일링
    - "/" 이 아니면 책 생성 버튼이보이지 않도록 구현

--- Frontend/src/components/Layout.tsx
+++ Frontend/src/components/Layout.tsx
@@ -1,11 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { FaPlus } from "react-icons/fa6";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+import { PATH } from "@/constants";
 
 const Layout = () => {
+    const location = useLocation();
+    const isHome = location.pathname === PATH.HOME;
+
     return (
-        <>
-            <div>Layout</div>
+        <main className="p-4">
+            <div className="flex justify-between">
+                <Link to={PATH.HOME} className="font-bold text-2xl">
+                    BookManager
+                </Link>
+                {isHome && (
+                    <Link
+                        to={PATH.CREATE}
+                        className="flex gap-2 h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 cursor-pointer"
+                    >
+                        Create Book
+                        <FaPlus />
+                    </Link>
+                )}
+            </div>
             <Outlet />
-        </>
+        </main>
     );
 };
 
