cbf5690d1e2d674d7ab9a2c7e43421a3cabbc3e4
임승현 <96limshyun@gmail.com>
2025-02-28 21:21:31 +0900

    feat: 페이지 네이션 구현

--- Backend/routes/book.ts
+++ Backend/routes/book.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { db } from "../app";
 
 const bookRouter = express.Router();
+const ITEMS_PER_PAGE = 10; 
 
 bookRouter.get("/books", (req, res) => {
     db.query('SELECT * FROM books', (err, results) => {
@@ -11,6 +12,25 @@ bookRouter.get("/books", (req, res) => {
         }
         res.json(results);
     });
+});
+
+bookRouter.get("/totalPage", (req, res) => {
+    const searchQuery = req.query.query ? `%${req.query.query}%` : "%";
+
+    db.query(
+        "SELECT COUNT(*) AS total FROM books WHERE bookname LIKE ? OR authors LIKE ?",
+        [searchQuery, searchQuery],
+        (err, results) => {
+            if (err) {
+                console.error("쿼리 실행 오류:", err);
+                return res.status(500).send("서버 오류");
+            }
+            const totalBooks = results[0].total;
+            const totalPages = Math.ceil(totalBooks / ITEMS_PER_PAGE);
+
+            res.json(totalPages);
+        }
+    );
 });
 
 export default bookRouter;
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,13 +1,19 @@
+import { useSearchParams } from "react-router-dom";
+
 import BookTable from "@/components/BookTable";
 import Pagination from "@/components/Pagination";
 import Search from "@/components/Search";
 
 const Home = () => {
+    const [searchParams] = useSearchParams()
+    const query = searchParams.get("query") || ""
+    const currentPage = Number(searchParams.get("page")) || 1
+    
     return (
         <section className="flex flex-col gap-6">
             <div className="flex justify-between items-center gap-2">
                 <Search />
-                <Pagination />
+                <Pagination query={query} currentPage={currentPage}/>
             </div>
                 <BookTable/>
         </section>
