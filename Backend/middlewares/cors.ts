bb3d09a330dabf1f8c7e578eec7f202b5f4b273b
임승현 <96limshyun@gmail.com>
2025-03-01 00:10:20 +0900

    fix: 서버 모든 접근 허용으로 수정

--- Backend/middlewares/cors.ts
+++ Backend/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-    origin: ["*"],
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 })
