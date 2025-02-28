c6817822b512f8d362317521bc86cec955852377
임승현 <96limshyun@gmail.com>
2025-02-28 20:34:31 +0900

    feat: tanstack query, tool 적용

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from "react-router-dom";
 
 import { route } from "./routers/routes";
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <RouterProvider router={route} />
-        </>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
