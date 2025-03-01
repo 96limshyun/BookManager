e94dfc32761b9912055ec56d3fe471ac4e2255d0
임승현 <96limshyun@gmail.com>
2025-03-01 22:02:12 +0900

    feat: react query 전역 에러 처리 설정 추가

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 
 import { route } from "./routers/routes";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            throwOnError: true,
+        },
+        mutations: {
+            throwOnError: true,
+        },
+    },
+});
 
 function App() {
     return (
